de92b6280df3230287ad98e49a6879a73d0382b6
Iso <kagiso.k.mashigo@gmial.com>
2020-11-09 20:34:42 +0000

    urls path added to server and views folder added

--- express_server.js
+++ express_server.js
@@ -17,10 +17,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+//  When sending variables to ejs template, it must be in an opbject
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
+})
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
