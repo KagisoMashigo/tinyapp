4a149067aefd18a44aa9c255df3639b4fdde82a6
Iso <kagiso.k.mashigo@gmial.com>
2020-11-10 20:29:24 +0000

    edit and delete links added to server and all relevant ejs files and functional

--- express_server.js
+++ express_server.js
@@ -33,7 +33,7 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.send("Ok");  // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -53,6 +53,15 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls")
+});
+
+app.post("/urls/:shortURL/update", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL; 
+  res.redirect("/urls")
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
